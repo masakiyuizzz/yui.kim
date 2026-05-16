a285d1dc77bc4973212115b3ab90a9c5747755eb
masakiyuizzz <234709575+masakiyuizzz@users.noreply.github.com>
2026-05-16 21:00:41 +0800

    [0.0.3]svg图标没有懒加载需求，而且有显示bug，直接删除

--- js/cards.js
+++ js/cards.js
@@ -9,7 +9,7 @@ const CardsModule = (function() {
         cardEl.innerHTML = `
             <div class="card-header">
                 <span class="card-icon">
-                    <img src="${card.icon}" alt="${card.iconAlt}" width="20" height="20" loading="lazy">
+                    <img src="${card.icon}" alt="${card.iconAlt}" width="20" height="20">
                 </span>
                 <h3 class="card-title">${escapeHtml(card.title)}</h3>
             </div>
